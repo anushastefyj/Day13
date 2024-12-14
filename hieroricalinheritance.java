66d7ec25712b747b75f8e3dd001cd9baddd9832d
anushastefyj <24hr5a3001@mtieat.org>
2024-12-14 12:22:36 +0530

    Add files via upload

---
+++ hieroricalinheritance.java
@@ -0,0 +1,26 @@
+import java.util.*;
+public class hieroricalinheritance{
+    public static void main(String args[]){
+        b s=new b();
+         s.print();
+        s.display();
+        c h=new c();
+       
+         h.show();
+    }
+}
+class a{
+    public void print(){
+        System.out.println("hello");
+    }
+}
+class b extends a{
+    public void display(){
+        System.out.println("anusha");
+    }
+}
+class c extends a{
+    public void show(){
+        System.out.println(" how are u");
+    }
+}
